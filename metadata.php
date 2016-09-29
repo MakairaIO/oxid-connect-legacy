a51a50420b4396696870e8e1b095b598d43a5502
Kore Nordmann <mail@kore-nordmann.de>
2016-09-29 15:37:13 +0200

    Added required base files

---
+++ metadata.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of a marmalade GmbH project
+ *
+ * It is not Open Source and may not be redistributed.
+ * For contact information please visit http://www.marmalade.de
+ *
+ * Version:    3.4
+ * Author URI: http://www.marmalade.de
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.1';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'makaira/connect',
+    'title'       => 'Makaira :: Connect',
+    'description' => 'Connector to Makaira',
+    'thumbnail'   => 'marmalade.jpg',
+    'version'     => '1.0.0',
+    'author'      => 'marmalade GmbH',
+    'url'         => 'http://www.marmalade.de',
+    'email'       => 'support@marmalade.de',
+    'extend'      => array(
+    ),
+    'files'       => array(
+    ),
+    'templates'   => array(),
+    'blocks'      => array(),
+    'events'      => array(),
+    'settings'    => array(),
+);
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Makaira :: Connect # 
+
+## English ##
+
+This implements the connector to Makaira
+
+### Licence Terms ###
+
+OXSEARCH underlies the marmalade licence terms.
+
+## Deutsch ##
+
+@TODO
+
+### Lizenzbestimmungen ###
+
+OXSEARCH unterliegt den Bestimmungen der Marmalade Kauflizenz.  
+
