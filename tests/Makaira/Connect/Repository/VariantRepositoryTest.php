732080aac5fe8665fb6269ce5e30c6c7b7419cab
Benjamin Kleiner <kleiner@marmalade.de>
2016-11-22 16:47:35 +0100

    Reworked category and variant repositories

--- tests/Makaira/Connect/Repository/VariantRepositoryTest.php
+++ tests/Makaira/Connect/Repository/VariantRepositoryTest.php
@@ -5,8 +5,6 @@ namespace Makaira\Connect\Repository;
 
 use Makaira\Connect\Change;
 use Makaira\Connect\DatabaseInterface;
-use Makaira\Connect\Result\Changes;
-use Makaira\Connect\Modifier;
 use Makaira\Connect\Type\Variant\Variant;
 
 class VariantRepositoryTest extends \PHPUnit_Framework_TestCase
@@ -20,28 +18,51 @@ class VariantRepositoryTest extends \PHPUnit_Framework_TestCase
         $databaseMock
             ->expects($this->once())
             ->method('query')
-            ->will($this->returnValue([['id' => 42, 'sequence' => 1, 'OXID' => 42]]));
+            ->will($this->returnValue([['id' => 42]]));
 
-        $changes = $repository->getChangesSince(0);
+        $modifiersMock
+            ->expects($this->any())
+            ->method('applyModifiers')
+            ->will($this->returnArgument(0));
 
+        $change = $repository->get(42);
         $this->assertEquals(
-            new Changes(
+            new Change(
                 array(
-                    'type'    => 'variant',
-                    'since'   => 0,
-                    'count'   => 1,
-                    'changes' => array(
-                        new Change(
-                            array(
-                                'sequence' => 1,
-                                'id'       => 42,
-                                'data'     => null,
-                            )
-                        ),
+                    'data' => new Variant(
+                        array(
+                            'id' => 42,
+                        )
                     ),
                 )
             ),
-            $changes
+            $change
+        );
+    }
+
+    public function testSetDeletedMarker()
+    {
+        $databaseMock = $this->getMock(DatabaseInterface::class);
+        $modifiersMock = $this->getMock(ModifierList::class);
+        $repository = new VariantRepository($databaseMock, $modifiersMock);
+
+        $databaseMock
+            ->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue([]));
+
+        $modifiersMock
+            ->expects($this->never())
+            ->method('applyModifiers');
+
+        $change = $repository->get(42);
+        $this->assertEquals(
+            new Change(
+                array(
+                    'deleted' => true,
+                )
+            ),
+            $change
         );
     }
 
@@ -54,39 +75,21 @@ class VariantRepositoryTest extends \PHPUnit_Framework_TestCase
         $databaseMock
             ->expects($this->once())
             ->method('query')
-            ->will($this->returnValue([['id' => 42, 'sequence' => 1, 'OXID' => 42]]));
+            ->will($this->returnValue([['id' => 42]]));
 
-        $product = new \stdClass();
         $modifiersMock
             ->expects($this->once())
             ->method('applyModifiers')
-            ->will($this->returnValue($product));
+            ->will($this->returnValue('modified'));
 
-        $changes = $repository->getChangesSince(0);
-
-        $this->assertSame(
-            $product,
-            $changes->changes[0]->data
+        $change = $repository->get(42);
+        $this->assertEquals(
+            new Change(
+                array(
+                    'data' => 'modified',
+                )
+            ),
+            $change
         );
-    }
-
-    public function testSetDeletedMarker()
-    {
-        $databaseMock = $this->getMock(DatabaseInterface::class);
-        $modifiersMock = $this->getMock(ModifierList::class);
-        $repository = new VariantRepository($databaseMock, $modifiersMock);
-
-        $databaseMock
-            ->expects($this->exactly(2))
-            ->method('query')
-            ->willReturnOnConsecutiveCalls(
-                $this->returnValue([['id' => 42, 'sequence' => 1, 'OXID' => null]]),
-                $this->returnValue([['OXID' => 42, 'TYPE' => 'variant']])
-            );
-
-        $changes = $repository->getChangesSince(0);
-
-        $this->assertEquals(true, $changes->changes[0]->deleted);
-        $this->assertEquals(null, $changes->changes[0]->data);
     }
 }
