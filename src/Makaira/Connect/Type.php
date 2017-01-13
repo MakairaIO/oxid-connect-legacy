3fe4af2ac1e1d9e7a49870803b95e9afe156d96b
Jens Richter <richter@marmalade.de>
2017-01-13 11:27:58 +0100

    Catch magic setter exceptions from data objects to simply drop unknown fields

--- src/Makaira/Connect/Type.php
+++ src/Makaira/Connect/Type.php
@@ -11,4 +11,14 @@ class Type extends DataObject
     public $shop = [];
     public $active = true;
     public $OXID;
+
+    public function __set($name, $value)
+    {
+        try {
+            parent::__set($name, $value);
+        } catch (\Exception $e) {
+            // catch exception on unknown fields, just drop them
+            // TODO: add field whitelisting for allowed additional fields
+        }
+    }
 }
