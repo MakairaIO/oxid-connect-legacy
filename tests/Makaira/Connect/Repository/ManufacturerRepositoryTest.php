a79ecf21ea68621b454d7f7a250f0845b8c4cda2
Kore Nordmann <mail@kore-nordmann.de>
2017-01-25 11:57:47 +0100

    Tested ManufacturerRepository

---
+++ tests/Makaira/Connect/Repository/ManufacturerRepositoryTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Makaira\Connect\Repository;
+
+use Makaira\Connect\Change;
+use Makaira\Connect\DatabaseInterface;
+use Makaira\Connect\Result\Changes;
+use Makaira\Connect\Type\Manufacturer\Manufacturer;
+use Makaira\Connect\Modifier;
+use Makaira\Connect\Repository\ModifierList;
+
+class ManufacturerRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLoadManufacturer()
+    {
+        $databaseMock = $this->getMock(DatabaseInterface::class);
+        $modifiersMock = $this->getMock(ModifierList::class);
+        $repository = new ManufacturerRepository($databaseMock, $modifiersMock);
+
+        $databaseMock
+            ->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue([['id' => 42]]));
+
+        $modifiersMock
+            ->expects($this->any())
+            ->method('applyModifiers')
+            ->will($this->returnArgument(0));
+
+        $change = $repository->get(42);
+        $this->assertEquals(
+            new Change(
+                array(
+                    'data' => new Manufacturer(
+                        array(
+                            'id' => 42,
+                        )
+                    ),
+                )
+            ),
+            $change
+        );
+    }
+
+    public function testSetDeletedMarker()
+    {
+        $databaseMock = $this->getMock(DatabaseInterface::class);
+        $modifiersMock = $this->getMock(ModifierList::class);
+        $repository = new ManufacturerRepository($databaseMock, $modifiersMock);
+
+        $databaseMock
+            ->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue([]));
+
+        $modifiersMock
+            ->expects($this->never())
+            ->method('applyModifiers');
+
+        $change = $repository->get(42);
+        $this->assertEquals(
+            new Change(
+                array(
+                    'deleted' => true,
+                )
+            ),
+            $change
+        );
+    }
+
+    public function testRunModifierLoadManufacturer()
+    {
+        $databaseMock = $this->getMock(DatabaseInterface::class);
+        $modifiersMock = $this->getMock(ModifierList::class);
+        $repository = new ManufacturerRepository($databaseMock, $modifiersMock);
+
+        $databaseMock
+            ->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue([['id' => 42]]));
+
+        $modifiersMock
+            ->expects($this->once())
+            ->method('applyModifiers')
+            ->will($this->returnValue('modified'));
+
+        $change = $repository->get(42);
+        $this->assertEquals(
+            new Change(
+                array(
+                    'data' => 'modified',
+                )
+            ),
+            $change
+        );
+    }
+
+    public function testGetAllIds()
+    {
+        $databaseMock = $this->getMock(DatabaseInterface::class);
+        $modifiersMock = $this->getMock(ModifierList::class);
+        $repository = new ManufacturerRepository($databaseMock, $modifiersMock);
+
+        $databaseMock
+            ->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue([['OXID' => 42]]));
+
+        $this->assertEquals([42], $repository->getAllIds());
+    }
+}
