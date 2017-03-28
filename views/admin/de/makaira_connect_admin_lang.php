ab0569d46498c807bd388fb3469c7785b831d179
Jens Richter <richter@marmalade.de>
2017-03-28 17:50:31 +0200

    Added translation key

--- views/admin/de/makaira_connect_admin_lang.php
+++ views/admin/de/makaira_connect_admin_lang.php
@@ -8,6 +8,7 @@ $aLang = array(
     'SHOP_MODULE_makaira_connect_secret'     => 'Shared Secret',
     'SHOP_MODULE_makaira_application_url'    => 'Makaira Url',
     'SHOP_MODULE_makaira_connect_load_limit' => 'Last Limitierung',
+    'SHOP_MODULE_makaira_connect_activate_search' => 'Für Suche aktivieren',
 
     'makaira_connect_iframe' => 'Makaira-Konfiguration',
 );
