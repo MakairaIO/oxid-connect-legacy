d4fb8de23107b9adad70ae821af623cf8c8db710
Jens Richter <richter@marmalade.de>
2017-08-28 15:40:27 +0200

    Removed dependency to oxsearch searchlinks

--- dic.php
+++ dic.php
@@ -122,14 +122,6 @@ $dic['makaira.connect.repository.manufacturer'] = function (\Marm\Yamm\DIC $dic)
     );
 };
 $dic->tag('makaira.connect.repository.manufacturer', 'makaira.connect.repository');
-$dic['makaira.connect.repository.searchlink'] = function (\Marm\Yamm\DIC $dic) {
-    return new Makaira\Connect\Repository\SearchLinkRepository(
-        $dic['oxid.database'], new Makaira\Connect\Repository\ModifierList(
-            $dic->getTagged('makaira.importer.modifier.searchlink')
-        )
-    );
-};
-$dic->tag('makaira.connect.repository.searchlink', 'makaira.connect.repository');
 
 // --------------------------------------
 
