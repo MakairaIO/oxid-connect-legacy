e46648314bbce3d079cc9b46f501e8e7c128853b
René Mäkeler <maekeler@marmalade.de>
2017-12-15 09:59:32 +0100

    Makaira-297 Fix filter.js breaking in IE because of missing forEach-polyfill

--- out/src/scripts/index.js
+++ out/src/scripts/index.js
@@ -1,3 +1,4 @@
+import Polyfills from './lib/polyfills'
 import Filters from './features/filters'
 import Autosuggestion from './features/autosuggestion'
 
---
+++ out/src/scripts/lib/polyfills.js
@@ -0,0 +1,10 @@
+// add polyfill for IE11
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    // eslint-disable-next-line
+    thisArg = thisArg || window;
+    for (let i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
