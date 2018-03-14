348a6f04ea73430f891a33baf0c27aa7e5d739ff
René Mäkeler <maekeler@marmalade.de>
2018-03-14 10:44:13 +0100

    Add prettier dependency

--- out/src/scripts/lib/polyfills.js
+++ out/src/scripts/lib/polyfills.js
@@ -3,27 +3,26 @@
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = function(callback, thisArg) {
     // eslint-disable-next-line
-    thisArg = thisArg || window;
+    thisArg = thisArg || window
     for (let i = 0; i < this.length; i++) {
-      callback.call(thisArg, this[i], i, this);
+      callback.call(thisArg, this[i], i, this)
     }
-  };
+  }
 }
 
 if (!Element.prototype.matches) {
   Element.prototype.matches =
-    Element.prototype.msMatchesSelector ||
-    Element.prototype.webkitMatchesSelector;
+    Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector
 }
 
 if (!Element.prototype.closest) {
   Element.prototype.closest = function(s) {
-    var el = this;
-    if (!document.documentElement.contains(el)) return null;
+    var el = this
+    if (!document.documentElement.contains(el)) return null
     do {
-      if (el.matches(s)) return el;
-      el = el.parentElement || el.parentNode;
-    } while (el !== null && el.nodeType === 1);
-    return null;
-  };
+      if (el.matches(s)) return el
+      el = el.parentElement || el.parentNode
+    } while (el !== null && el.nodeType === 1)
+    return null
+  }
 }
