5eaeb5df0a7ea37cc8b3d5ac06513685bf32d33f
Thomas Uhlig <uhlig@marmalade.de>
2018-04-19 10:15:07 +0200

    fixed english translation

--- views/admin/en/makaira_connect_admin_lang.php
+++ views/admin/en/makaira_connect_admin_lang.php
@@ -1,19 +1,20 @@
 <?php
 
-$sLangName  = "English";
+$sLangName = "English";
 
 $aLang = array(
-    'charset'                                     => 'UTF-8',
-    'SHOP_MODULE_GROUP_SETTINGS'                  => 'Settings',
-    'SHOP_MODULE_makaira_connect_secret'          => 'Shared Secret',
-    'SHOP_MODULE_makaira_application_url'         => 'Makaira Url',
-    'SHOP_MODULE_makaira_connect_load_limit'      => 'Load Limitation',
-    'HELP_SHOP_MODULE_makaira_connect_load_limit' => 'Common value: Half of the number of CPU cores on this machine (1 core => .5; 4 cores => 2)',
-    'SHOP_MODULE_makaira_instance'                => 'Instance',
-    'SHOP_MODULE_makaira_connect_activate_search' => 'Activate for search',
-    'SHOP_MODULE_makaira_connect_activate_listing' => 'Activate for category',
+    'charset'                                          => 'UTF-8',
+    'SHOP_MODULE_GROUP_SETTINGS'                       => 'Settings',
+    'SHOP_MODULE_makaira_connect_secret'               => 'Shared Secret',
+    'SHOP_MODULE_makaira_application_url'              => 'Makaira Url',
+    'SHOP_MODULE_makaira_connect_load_limit'           => 'Load Limitation',
+    'HELP_SHOP_MODULE_makaira_connect_load_limit'      => 'Common value: Half of the number of CPU cores on this machine (1 core => .5; 4 cores => 2)',
+    'SHOP_MODULE_makaira_instance'                     => 'Instance',
+    'SHOP_MODULE_makaira_connect_activate_search'      => 'Activate for search',
+    'SHOP_MODULE_makaira_connect_activate_listing'     => 'Activate for category',
     'SHOP_MODULE_makaira_connect_category_inheritance' => 'Parent category contains all products from child categories',
-    'SHOP_MODULE_makaira_connect_seofilter'       => 'Generate SEO url for filtered category and manufacturer pages',
+    'SHOP_MODULE_makaira_connect_categorytree_id'      => 'ID of Categorytree filter (if exists)',
+    'SHOP_MODULE_makaira_connect_seofilter'            => 'Generate SEO url for filtered category and manufacturer pages',
 
     'SHOP_MODULE_GROUP_IMPORTFIELDS'                   => 'Import Fields',
     'SHOP_MODULE_makaira_field_blacklist_product'      => '<b>Products</b>: blacklisted fields',
