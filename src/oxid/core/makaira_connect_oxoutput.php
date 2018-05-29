e5cbc6d63a25c1f3d08df8aea4dee2a783fc432c
Jens Richter <richter@marmalade.de>
2018-05-29 13:29:19 +0200

    Cleanup oxoutput overwrite

--- src/oxid/core/makaira_connect_oxoutput.php
+++ src/oxid/core/makaira_connect_oxoutput.php
@@ -20,25 +20,19 @@ class makaira_connect_oxoutput extends makaira_connect_oxoutput_parent
      */
     public function output($sName, $output)
     {
-        switch ($this->_sOutputFormat) {
-            case self::OUTPUT_FORMAT_JSON:
-                $this->_aBuffer[$sName] = $output;
-                break;
-            case self::OUTPUT_FORMAT_HTML:
-            default:
-                $closingHead = "</body>";
-                $closingHeadNew = "<script type=\"text/javascript\">
+        if (self::OUTPUT_FORMAT_HTML === $this->_sOutputFormat) {
+            $closingHead = "</body>";
+            $closingHeadNew = "<script type=\"text/javascript\">
 oiOS=new Date().getTimezoneOffset();oiOS=(oiOS<0?\"+\":\"-\")+(\"00\"+parseInt((Math.abs(oiOS/60)))).slice(-2);
 document.cookie= \"oiLocalTimeZone=\"+oiOS+\";path=/;\";
 </script></body>";
 
-                $output = ltrim($output);
-                if (false !== ($pos = stripos($output, $closingHead))) {
-                    $output = substr_replace($output, $closingHeadNew, $pos, strlen($closingHead));
-                }
-
-                echo $output;
-                break;
+            $output = ltrim($output);
+            if (false !== ($pos = stripos($output, $closingHead))) {
+                $output = substr_replace($output, $closingHeadNew, $pos, strlen($closingHead));
+            }
         }
+
+        parent::output($sName, $output);
     }
 }
