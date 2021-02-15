b8249ae0a551d46dfdba08ac4f8ec22199990155
Florian Ludwig <ludwig@marmalade.de>
2021-02-15 15:32:09 +0100

    Fixes issues with econda personalization where no visitor id was created after first page call

--- out/src/scripts/features/econda.js
+++ out/src/scripts/features/econda.js
@@ -1,16 +1,11 @@
-function hCreateCookie(name, value, lifetime, dom) {
-    var expires = "";
-    if (lifetime) {
-        var date = new Date();
-        date.setTime(date.getTime() + 1000 * lifetime);
-        expires = " expires=" + date.toGMTString() + ";";
-    }
-    var domain = "";
-    if (dom) {
-        domain = " domain=" + dom;
-    }
+function hCreateCookie(name, value) {
+    const lifetime = 60 * 60 * 365; // 1 year
+    let expires = "";
+    let date = new Date();
+    date.setTime(date.getTime() + 1000 * lifetime);
+    expires = " expires=" + date.toGMTString() + ";";
 
-    document.cookie = name + "=" + value + ";" + expires + " path=/;" + domain + ";";
+    document.cookie = name + "=" + value + ";" + expires + " path=/;";
 }
 
 function hReadCookie(name) {
@@ -33,51 +28,54 @@ function hDeleteCookie(name) {
     hCreateCookie(name, "", -1);
 }
 
+function createEcondaSession(makEcondaAccountId) {
+    const makCookieSession = "mak_econda_session";
+    const makEcondaSession = hReadCookie(makCookieSession);
+
+    if (makEcondaSession) {
+        return;
+    }
+
+    let makEcondaRequest = new econda.recengine.Request({accountId: makEcondaAccountId});
+    let makEcondaParams = makEcondaRequest.getRecommendationServiceParameters();
+    const makCookieValue = JSON.stringify(makEcondaParams);
+
+    hCreateCookie(makCookieSession, makCookieValue);
+}
+
 function initEcondaCookie() {
-    if ("undefined" !=
-        (
-            typeof econda
-        )) {
-        var makCookiePrefix = "mak_";
-        var makCookieAccountId = makCookiePrefix + "econda_aid";
-        var makCookieSession = makCookiePrefix + "econda_session";
-        var makCookieLifetime = 86400;
-        var makCookieDomain = null;
-        var makCookieValue = null;
-
-        var makEcondaAccountId = hReadCookie(makCookieAccountId);
-
-        if (makEcondaAccountId == null) {
-            var request = new XMLHttpRequest()
-            request.open('GET', `?cl=makaira_connect_econda`, true)
-
-            request.onload = () => {
-                if (request.status >= 200 && request.status < 400) {
-                    makEcondaAccountId = request.responseText;
-                    makCookieValue = btoa(makEcondaAccountId);
-
-                    hCreateCookie(makCookieAccountId, makCookieValue, makCookieLifetime, makCookieDomain);
-                }
-            }
-
-            request.onerror = () => {
-                // There was a connection error of some sort
-            }
-
-            request.send();
-        } else {
-            makEcondaAccountId = atob(makEcondaAccountId);
-        }
-
-        if (makEcondaAccountId != null) {
-            var makEcondaRequest = new econda.recengine.Request({accountId: makEcondaAccountId});
-            var makEcondaParams = makEcondaRequest.getRecommendationServiceParameters();
-            makCookieValue = JSON.stringify(makEcondaParams);
-
-            hCreateCookie(makCookieSession, makCookieValue, makCookieLifetime, makCookieDomain);
-        }
-    } else {
+    if ("undefined" === typeof econda) {
         hDeleteCookie("mak_econda_session");
+        return;
+    }
+
+    const makCookieAccountId = "mak_econda_aid";
+    let makCookieValue = null;
+
+    let makEcondaAccountId = hReadCookie(makCookieAccountId);
+
+    if (makEcondaAccountId == null) {
+        var request = new XMLHttpRequest()
+        request.open('GET', `?cl=makaira_connect_econda`, true);
+
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 400) {
+                makEcondaAccountId = request.responseText;
+                makCookieValue = btoa(makEcondaAccountId);
+
+                hCreateCookie(makCookieAccountId, makCookieValue);
+                createEcondaSession(makEcondaAccountId);
+            }
+        }
+
+        request.onerror = () => {
+            // There was a connection error of some sort
+        }
+
+        request.send();
+    } else {
+        makEcondaAccountId = atob(makEcondaAccountId);
+        createEcondaSession(makEcondaAccountId);
     }
 }
 
